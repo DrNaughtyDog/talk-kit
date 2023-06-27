deab5ef2fdfd556ea2d581aed8384b8f13232ed9
Vincent Welker <welker@atix.de>
2023-06-27 11:10:41 +0200

    scripts: Update autoscaler.sh, hardcode values

--- scripts/autoscaler.sh
+++ scripts/autoscaler.sh
@@ -1,6 +1,6 @@
 helm repo add cluster-autoscaler https://kubernetes.github.io/autoscaler
 kubectl create ns autoscaler
 helm upgrade --install cluster-autoscaler cluster-autoscaler/cluster-autoscaler -n autoscaler --values=autoscaler.yml \
-  --set autoDiscovery.clusterName=$(cd ../terraform; terraform output -raw cluster_name)\
-  --set awsRegion=$(cd ../terraform; terraform output -raw region)\
-  --set rbac.serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=$(cd ../terraform; terraform output -raw autoscaler_iam_role)
+  --set autoDiscovery.clusterName="autoscale" \
+  --set awsRegion="us-east-2" \
+  --set rbac.serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="arn:aws:iam::410793487517:role/autoscale-cluster-autoscaler"
